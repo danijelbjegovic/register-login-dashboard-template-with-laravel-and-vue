8b8c832451d11fc7c08b38a42b9100d75f2ef053
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-21 23:47:24 +0200

    Add signup function to authcontroller, make exception in middleware for it and add route for it

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -13,7 +14,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
     }
 
     /**
@@ -30,6 +31,11 @@ class AuthController extends Controller
         }
 
         return $this->respondWithToken($token);
+    }
+    public function signup(Request $request)
+    {
+        User::create($equest->all());
+        return $this->login($request);
     }
 
     /**
