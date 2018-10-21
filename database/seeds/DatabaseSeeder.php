55fca26dcae228747a612924e0c69ab8e5171c56
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-21 22:42:56 +0200

    Seed users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class,10)->create();
     }
 }
