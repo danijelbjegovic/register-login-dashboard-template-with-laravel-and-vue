260cd520e77c45fb4dc4ad7afbcf07fc5c3fd769
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-23 14:37:08 +0200

    install npm and vuetify, then configure app.js and app.scss

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,10 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+ 
+Vue.use(Vuetify)
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
