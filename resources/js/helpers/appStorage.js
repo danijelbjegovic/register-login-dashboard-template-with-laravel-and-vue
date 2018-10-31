40f49175c943b9d3a112720b5541b492fe99ba32
Danijel Bjegovic <danijelbjegovic95@gmail.com>
2018-10-31 13:55:51 +0100

    Add AppStorage class that will handle token storage

---
+++ resources/js/helpers/appStorage.js
@@ -0,0 +1,26 @@
+class AppStorage{
+    storeToken(token){
+        return localStorage.setItem('token', token); 
+    }
+    storeUser(user){
+        localStorage.setItem('user', user);
+    }
+    store(user, token){
+        this.storeToken(token)
+        this.storeUser(user)
+    }
+
+    clear(){
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
+    }
+
+    getToken(){
+        return localStorage.getItem('token')
+    }
+    getUser(){
+        return localStorage.getItem('user')
+    }
+}
+
+export default AppStorage = new AppStorage()
